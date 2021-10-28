33e386e9742c354397b8221d0ba61d6a701c75e4
Daniel Vilela <dmvvilela@gmail.com>
2021-10-28 15:03:42 -0300

    styling pipes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 test("renders without crashing", () => {
-  render(<App />);
+  // render(<App />);
   // const linkElement = screen.getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
 });
--- src/components/PipeList.tsx
+++ src/components/PipeList.tsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
-import { Pipes } from "../graphql/__generated__/Pipes"; // Import
+import { Pipes } from "../graphql/__generated__/Pipes";
+import { loader } from "graphql.macro";
+
+const pipesQuery = loader("../graphql/pipes.graphql");
+
+const TailwindColor = require("@videsk/tailwind-random-color");
 
 const PipeList = () => {
-  // const {
-  //   loading: isLoading,
-  //   data: pipesConnection,
-  //   error: pipesError,
-  // } = useQuery<Pipes>(GET_ALL_TODOS); // Use the type here for type safety
+  const {
+    loading: isLoading,
+    data: pipesConnection,
+    error: pipesError,
+  } = useQuery<Pipes>(pipesQuery);
 
-  // if (isLoading) return <div>Loading...</div>;
-  // if (pipesError)
-  //   return <div>An error occurred {JSON.stringify(pipesError)}</div>;
-  // if (!pipesConnection) return <div>None</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (pipesError)
+    return <div>An error occurred {JSON.stringify(pipesError)}</div>;
+  if (!pipesConnection) return <div>None</div>;
 
-  // const pipes = pipesConnection.organizations?.map((t) => t?.name);
+  // For this exercise, get only the first organization
+  const pipes = pipesConnection.organizations?.map((t) => t?.pipes)[0];
+  console.log(pipes);
 
   return (
     <section>
-      <div className="grid place-items-center">
+      <div className="grid mb-16 place-items-center">
         <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
           <h1 className="col-span-2 text-4xl font-extrabold md:col-span-4">
             Your Pipes
@@ -27,12 +34,24 @@ const PipeList = () => {
             Here are all your processes
           </h2>
           {/* TODO: w-auto */}
-          <div className="w-32 bg-blue-500 pipe"></div>
-          <div className="w-32 bg-pink-500 pipe"></div>
-          <div className="w-32 bg-blue-500 pipe"></div>
-          <div className="w-32 bg-pink-500 pipe"></div>
-          <div className="w-32 bg-blue-500 pipe"></div>
-          <div className="w-32 bg-pink-500 pipe"></div>
+          {pipes?.map((pipe) => (
+            <div
+              key={pipe?.id}
+              className={`p-2 rounded-xl shadow-sm hover:shadow-md flex flex-col w-36 cursor-pointer ${new TailwindColor().pick()} bg-opacity-10 pipe`}
+            >
+              <img
+                src="pipe.png"
+                className="self-center w-12 m-4"
+                alt="pipe-icon"
+              />
+              <h3 className="flex-1 text-sm font-bold text-center">
+                {pipe?.name}
+              </h3>
+              <h3 className="flex-1 p-1 text-xs font-light text-center opacity-40">
+                {pipe?.cards_count} {pipe?.cards_count === 1 ? "card" : "cards"}
+              </h3>
+            </div>
+          ))}
         </div>
       </div>
     </section>
